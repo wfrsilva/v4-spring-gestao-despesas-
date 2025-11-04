d0f684b5fc19dc0fe6435e41b42ea8270eb55564
Wendel Silva <wfrsilva@gmail.com>
2025-11-04 20:17:24 -0300

    Primeiro commit

---
+++ src/main/java/dev/wfrsilva/gestao_despesas/GestaoDeDespesasPessoaisApplication.java
@@ -0,0 +1,13 @@
+package dev.wfrsilva.gestao_despesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeDespesasPessoaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeDespesasPessoaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wfrsilva/gestao_despesas/GestaoDeDespesasPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wfrsilva.gestao_despesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeDespesasPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
