32ccd0ac626ba2c4934a97096ebe5e9bcdf27158
Wendel Silva <wfrsilva@gmail.com>
2025-11-04 20:36:07 -0300

    GestaoDespesaController.java criada
     Changes to be committed:
    	new file:   src/main/java/dev/wfrsilva/gestao_despesas/controller/GestaoDespesaController.java
    
     Untracked files:
    	src/main/java/dev/wfrsilva/gestao_despesas/custom_message/
    	src/main/java/dev/wfrsilva/gestao_despesas/entity/
    	src/main/java/dev/wfrsilva/gestao_despesas/useCase/

---
+++ src/main/java/dev/wfrsilva/gestao_despesas/controller/GestaoDespesaController.java
@@ -0,0 +1,52 @@
+package dev.wfrsilva.gestao_despesas.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.wfrsilva.gestao_despesas.custom_message.ErrorMessage;
+import dev.wfrsilva.gestao_despesas.entity.Despesa;
+import dev.wfrsilva.gestao_despesas.useCase.BuscarDespesaUseCase;
+import dev.wfrsilva.gestao_despesas.useCase.CadastroDespesaUseCase;
+
+@RequestMapping("/gestao")
+@RestController
+public class GestaoDespesaController {
+
+    @Autowired
+    CadastroDespesaUseCase cadastroDespesaUseCase;
+
+    @Autowired
+    BuscarDespesaUseCase buscarDespesaUseCase;
+
+    @PostMapping("/create")
+    public ResponseEntity <?> create(@RequestBody Despesa despesa)
+    {
+        try
+        {
+            var result = cadastroDespesaUseCase.execute(despesa);
+            return ResponseEntity.ok(result);
+        }//try
+        catch(IllegalArgumentException e)
+        {
+            var errorMessage = new ErrorMessage(e.getMessage(), "INVALID_PARAMS");
+            return ResponseEntity.status(400).body(errorMessage);
+        }//catch
+
+    }//create
+
+    @GetMapping("/{email}")
+    public List <Despesa> findByEmailAndDate(@PathVariable String email, @RequestParam(required = false) LocalDate data)
+    {
+        return buscarDespesaUseCase.execute(email,data);
+    }//findByEmailAndDate
+
+}//GestaoDespesaController
