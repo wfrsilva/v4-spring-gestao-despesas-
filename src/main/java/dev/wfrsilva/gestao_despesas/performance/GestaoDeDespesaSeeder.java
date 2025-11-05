8e2869250d3f6580d7fa35daf636198b259c7f77
Wendel Silva <wfrsilva@gmail.com>
2025-11-05 18:51:03 -0300

    GestaoDeDespesaSeeder.java criada
     Changes to be committed:
    	new file:   src/main/java/dev/wfrsilva/gestao_despesas/performance/GestaoDeDespesaSeeder.java

---
+++ src/main/java/dev/wfrsilva/gestao_despesas/performance/GestaoDeDespesaSeeder.java
@@ -0,0 +1,46 @@
+package dev.wfrsilva.gestao_despesas.performance;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import dev.wfrsilva.gestao_despesas.entity.Despesa;
+import dev.wfrsilva.gestao_despesas.repository.DespesaRepository;
+
+@Component
+public class GestaoDeDespesaSeeder implements CommandLineRunner{
+
+    @Autowired
+    DespesaRepository despesaRepository;
+
+    @Override
+    public void run(String ... args) throws Exception
+    {   
+        List <Despesa> despesas = new ArrayList <>();
+
+        System.out.println("Iniciando geração de seed");
+
+        for (int i = 0; i <= 150000; i++)
+        {
+            Despesa despesa = new Despesa();
+            despesa.setDescricao("Gasto n: " + i);
+            despesa.setValor(BigDecimal.valueOf(10 + (i % 50)));
+            despesa.setData(LocalDate.now().minusDays((i % 30)));
+            despesa.setCategoria("TESTE");
+            despesa.setEmail("v4-performance@gmail.com");
+
+            despesas.add(despesa);
+        }//for
+
+        despesaRepository.saveAll(despesas);
+
+        System.out.println("Finalizou geração de seed");
+
+    }//run
+    
+}//GestaoDeDespesaSeeder
